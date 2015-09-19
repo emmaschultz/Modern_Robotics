07594396148a46be628da09f2347ac90f7402638
emmaschultz <ecs89@case.edu>
2015-09-19 15:01:52 -0400

    Updated package.xml with new dependencies. Edited a few comments in velocity_commander

--- velocity_project/src/velocity_commander.cpp
+++ velocity_project/src/velocity_commander.cpp
@@ -9,9 +9,10 @@ std_msgs::Float64 g_frequency = 1.0;
 
 bool callback(velocity_project::amplitude_frequency_msgRequest& request, velocity_project::amplitude_frequency_msgResponse& response) {
     ROS_INFO("callback activated");
-    g_amplitude.data = request.amplitude;
-    g_frequency.data = request.frequency;
+    g_amplitude.data = request.amplitude;  //set amplitude equal to value specified by client
+    g_frequency.data = request.frequency;  //set frequency equal to value specified by client
 
+    //return true to the client to signal that the task has been completed
     response.amplitude_changed = true;
     response.frequency_changed = true;
     
@@ -21,18 +22,12 @@ bool callback(velocity_project::amplitude_frequency_msgRequest& request, velocit
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "velocity_commander1");
-    ros::init(argc, argv, "amplitude_frequency_service1"); //TODO: is this necessary?
+    //ros::init(argc, argv, "amplitude_frequency_service1"); //TODO: is this necessary?
     ros::NodeHandle n;
     ros::Publisher my_commander_object = n.advertise<std_msgs::Float64>("vel_cmd", 1);  //publish to vel_cmd topic
     ros::ServiceServer amplitude_frequency_service = n.advertiseService("change_amplitude_and_frequency", callback);
-    
-    //ros::spin()   //TODO: this was in the example service. does this need to be anywhere in this node? or maybe this needs to be a ros::spinOnce()?
 
     std_msgs::Float64 command; //this will contain the value that will be published to vel_cmd
-   
-    //variables to determine vel_cmd
-    //double amplitude = 1.0;
-    //double frequency = 1.0;
     double point_in_time = 0.0;
 
     ros::Rate naptime(100.0);   //increased this to have higher sample rate, which results in a smoother sin wave
