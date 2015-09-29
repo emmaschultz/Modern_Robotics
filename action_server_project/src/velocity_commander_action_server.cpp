6f489f84846243cc2aa74fdc83d3f18a7a61d2a3
emmaschultz <ecs89@case.edu>
2015-09-29 19:07:18 -0400

    Added in code to account for a certain number of cycles. Untested at this point.

--- action_server_project/src/velocity_commander_action_server.cpp
+++ action_server_project/src/velocity_commander_action_server.cpp
@@ -45,20 +45,6 @@ void VelocityCommanderActionServer::executeCB(const actionlib::SimpleActionServe
     as_.setSucceeded(result_);
 }
 
-/*
-bool callback(velocity_project::amplitude_frequency_msgRequest& request, velocity_project::amplitude_frequency_msgResponse& response) {
-    ROS_INFO("callback activated");
-    g_amplitude.data = request.amplitude;  //set amplitude equal to value specified by client
-    g_frequency.data = request.frequency;  //set frequency equal to value specified by client
-
-    //return true to the client to signal that the task has been completed
-    response.amplitude_changed = true;
-    response.frequency_changed = true;
-    
-    return true;
-}
-*/
-
 int main(int argc, char **argv) {
     ros::init(argc, argv, "velocity_commander_action_server_node");  //name of this node
     ros::Publisher my_commander_object = n.advertise<std_msgs::Float64>("vel_cmd", 1);  //publish to vel_cmd topic
@@ -67,18 +53,35 @@ int main(int argc, char **argv) {
     VelocityCommanderActionServer actionServerObject;   //create an instance of VelocityCommanderActionServer
 
     std_msgs::Float64 command; //this will contain the value that will be published to vel_cmd
+    std_msgs::Float64 period;  //this will house the data containing the length of the period for the sine wave
     double point_in_time = 0.0;
+    double totalPeriods = 0.0;
+    double counter = 0.0;
 
     ros::Rate naptime(100.0);   //increased this to have higher sample rate, which results in a smoother sin wave
 
     command.data = 0.0; //initialze the vel_cmd to 0
 
     while (ros::ok()) {
-        ros::spinOnce(); //update data from callback function
-        command.data = g_amplitude.data * sin(2 * PI * g_frequency.data * point_in_time);  //calculate the sin wave values
-        point_in_time += 0.01;  //increment the counter; this should execute 100 times in one second
-        my_commander_object.publish(command);  //publishes the current vel_cmd
+        ros::spinOnce();  //update to the most current data
+
+        period.data = (2 * PI)/g_frequency.data;       //determine the length of one period of the sine wave
+        totalPeriods = period.data * g_numCycles.data; //determine the length of all of the periods (cycles) based on the number of cycles requested
+
+        //if the number of cycles has not been reached yet, continue calculating the sine wave
+        //else command a velocity of zero
+        if(totalPeriods > counter){
+            counter += 0.01;
+            command.data = g_amplitude.data * sin(2 * PI * g_frequency.data * point_in_time);
+        } else {
+            counter = 0.0;  //reset counter back to zero because last cycle has been reached
+            command.data = 0.0; //command a velocity of zero when the requested number of cycles have been completed
+        }
+
+        point_in_time += 0.01;
+        my_commander_object.publish(command);
         naptime.sleep();
     }
+
 }
 
